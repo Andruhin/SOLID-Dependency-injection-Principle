e6884de7fb9303aebb6da6032162fcaf71fc8d7b
Andriy Ksenych <andruhaha@gmail.com>
2025-05-19 13:18:09 +0300

    Implemented feature 2

--- src/main/java/org/kotlin/test2/Main.java
+++ src/main/java/org/kotlin/test2/Main.java
@@ -46,6 +46,7 @@ public class Main {
         System.out.println("");
         System.out.println("");
         System.out.println("!!!!!   Second Implementation    !!!!!");
+        System.out.println("!!!!!   Second Implementation    !!!!!");
         //Second implementation
         ServiceLocator serviceLocator = new ServiceLocator();
         GetItemsUseCase injectedGetItemsUseCase = serviceLocator.provideGetItemsUseCase();
